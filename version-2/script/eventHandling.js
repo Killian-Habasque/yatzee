1155a94a686697267cc3f21e50709116a3896450
Killian Habasque <killian.habasque@gmail.com>
2024-02-21 13:39:57 +0100

    feat scale & cursor on dice hover

--- version-2/script/eventHandling.js
+++ version-2/script/eventHandling.js
@@ -21,20 +21,62 @@ export function onDocumentMouseDown(event) {
         // console.log(selectedObject)
         if (selectedObject.type === "Group") {
             selectedObject.callback()
-            // Vérification si l'objet parent a des enfants
-            if (selectedObject.children.length > 0) {
-                // Parcourir tous les enfants de l'objet parent
-                selectedObject.children.forEach(child => {
-                    selectedObject.rotation.set(0, 0, 0);
-                    if (child.scale.x === 1.2) {
-                        child.scale.set(1, 1, 1); // Changer l'échelle de l'enfant à 1
-                    } else {
-                        // console.log(selectedObject)
+  
+            // if (selectedObject.children.length > 0) {
+            //     // Parcourir tous les enfants de l'objet parent
+            //     selectedObject.children.forEach(child => {
+            //         selectedObject.rotation.set(0, 0, 0);
+            //         if (child.scale.x === 1.2) {
+            //             child.scale.set(1, 1, 1); 
+            //         } else {
+            //             // console.log(selectedObject)
 
-                        child.scale.set(1.2, 1.2, 1.2); // Changer l'échelle de l'enfant à 1.2
-                    }
-                });
-            }
+            //             child.scale.set(1.2, 1.2, 1.2); 
+            //         }
+            //     });
+            // }
         }
     }
 }
+
+let hoveredObject = null; 
+
+export function onDocumentMouseMove(event) {
+    var raycaster = new THREE.Raycaster();
+    var mouse = new THREE.Vector2();
+    event.preventDefault();
+    mouse.x = (event.clientX / gameData.renderer.domElement.clientWidth) * 2 - 1;
+    mouse.y = - (event.clientY / gameData.renderer.domElement.clientHeight) * 2 + 1;
+    raycaster.setFromCamera(mouse, gameData.camera);
+    var intersects = raycaster.intersectObjects(gameData.scene.children, true); 
+
+    // Réinitialiser l'échelle des objets qui ne sont plus survolés
+    if (hoveredObject && !intersects.find(intersect => intersect.object === hoveredObject)) {
+        hoveredObject.children.forEach(child => {
+            if (child.type === "Mesh") {
+                child.scale.set(1, 1, 1);
+            }
+        });
+        document.body.style.cursor = 'auto'; 
+        hoveredObject = null;
+    }
+
+    if (intersects.length > 0) {
+        const object = intersects[0].object;
+        if (object.parent.type === "Group") {
+            object.parent.children.forEach(child => {
+                if (child.type === "Mesh") {
+                    child.scale.set(1.1, 1.1, 1.1); 
+                }
+            });
+            document.body.style.cursor = 'pointer'; 
+            hoveredObject = object.parent;
+        }
+    } else {
+        document.body.style.cursor = 'auto'; 
+    }
+}
+
+
+document.addEventListener('mousemove', onDocumentMouseMove, false);
+// document.removeEventListener('mousemove', onDocumentMouseMove, false);
