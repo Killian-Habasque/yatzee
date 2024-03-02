a6cffc2f2eb54758357591e03a433631fe5aefe6
Killian Habasque <killian.habasque@gmail.com>
2024-03-02 15:53:30 +0100

    feat(sound): add togle menu

--- script/class/hud/Sound.js
+++ script/class/hud/Sound.js
@@ -1,25 +1,37 @@
 export default class Sound {
     constructor() {
-        window.addEventListener("DOMContentLoaded", this.createMusic);
+        window.addEventListener("DOMContentLoaded", this.createMusic(this));
+
+        this.toggleBtnSoundMenu()
+        this.soundEnabled = localStorage.getItem("soundEnabled") === "true";
     }
 
+    toggleBtnSoundMenu() {
+        let element = document.getElementById("btn-audio");
+        element.addEventListener('click', (e) => {
+
+            element.classList.toggle("active");
+            this.soundEnabled = !this.soundEnabled;
+            console.log(this.soundEnabled)
+            localStorage.setItem("soundEnabled", this.soundEnabled);
+            if (this.soundEnabled) {
+                this.audioElement.play();
+            } else {
+                this.audioElement.pause();
+            }
+        })
+    }
     createMusic() {
-            let audioElement = document.createElement("audio");
-            audioElement.setAttribute("src", "./assets/songs/music.mp3");
-            audioElement.setAttribute("preload", "auto");
-            audioElement.setAttribute("autoplay", "");
-            // audioElement.setAttribute("controls", "");
-            audioElement.setAttribute("loop", "");
-            audioElement.textContent = "Your browser does not support the audio element.";
-        
-            let container = document.querySelector("header");
-            container.appendChild(audioElement);
-    }
+        this.audioElement = document.createElement("audio");
+        this.audioElement.setAttribute("src", "./assets/songs/music.mp3");
+        this.audioElement.setAttribute("preload", "auto");
+        this.audioElement.setAttribute("loop", "");
 
-    addSongToLocalStorage() {
-        if (!localStorage.getItem("song")) {
-            localStorage.setItem("song", "true");
+        if (this.soundEnabled) {
+            this.audioElement.play();
         }
+
+        let container = document.querySelector("header");
+        container.appendChild(this.audioElement);
     }
-    
 }
