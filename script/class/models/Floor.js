74b917bae0ad23ff59d2920c81dcf8e8c913edbe
Killian Habasque <killian.habasque@gmail.com>
2024-03-06 18:23:42 +0100

    refactor(Floor): clean createFloor function

--- script/class/models/Floor.js
+++ script/class/models/Floor.js
@@ -37,48 +37,31 @@ export default class Floor {
     Création du plane
     */
     createFloor() {
-        var textureSize = 25;
-        // 2
-        var groundTexture = new THREE.TextureLoader().load('/assets/images/textures/wood_floor_worn_diff_2k.png');
-        groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
-        groundTexture.repeat.set(textureSize, textureSize);
-        // groundTexture.anisotropy = 16;
-
-        var groundTexture2 = new THREE.TextureLoader().load('/assets/images/textures/wood_floor_worn_nor_gl_2k.jpg');
-        groundTexture2.wrapS = groundTexture2.wrapT = THREE.RepeatWrapping;
-        groundTexture2.repeat.set(textureSize, textureSize);
-
-
-        var groundTexture3 = new THREE.TextureLoader().load('/assets/images/textures/wood_floor_worn_arm_1k.png');
-        groundTexture3.wrapS = groundTexture3.wrapT = THREE.RepeatWrapping;
-        groundTexture3.repeat.set(textureSize, textureSize);
-
-        var groundTexture4 = new THREE.TextureLoader().load('/assets/images/textures/wood_floor_worn_rough_1k.png');
-        groundTexture4.wrapS = groundTexture4.wrapT = THREE.RepeatWrapping;
-        groundTexture4.repeat.set(textureSize, textureSize);
-
-        var groundTexture5 = new THREE.TextureLoader().load('/assets/images/textures/planks//wood_planks_disp_1k.png');
-        groundTexture5.wrapS = groundTexture5.wrapT = THREE.RepeatWrapping;
-        groundTexture5.repeat.set(textureSize, textureSize);
-
-        const floor = new THREE.Mesh(
-            new THREE.PlaneGeometry(1000, 1000, 100, 100), // 4 x 100
-            new THREE.MeshStandardMaterial({ // Utilisation de MeshStandardMaterial
-                map: groundTexture,
-                normalMap: groundTexture2,
-                aoMap: groundTexture3,
-                color: '#FFFFFF',
-                metalness: 0.025,
-                roughness: 1,
-                roughnessMap: groundTexture4,
-                side: THREE.DoubleSide,
-                ambient: new THREE.Color(0x808080)
-            })
-        );
-        floor.geometry.addAttribute('uv2', new THREE.BufferAttribute(floor.geometry.attributes.uv.array, 2));
+        const textureDiff = this.loadTexture('/assets/images/textures/wood_floor_worn_diff_2k.png');
+        const textureNor = this.loadTexture('/assets/images/textures/wood_floor_worn_nor_gl_2k.jpg');
+        const textureArm = this.loadTexture('/assets/images/textures/wood_floor_worn_arm_1k.png');
+        const textureRough = this.loadTexture('/assets/images/textures/wood_floor_worn_rough_1k.png');
+    
+        const floorGeometry = new THREE.PlaneGeometry(1000, 1000, 100, 100);
+        floorGeometry.addAttribute('uv2', new THREE.BufferAttribute(floorGeometry.attributes.uv.array, 2));
+    
+        const floorMaterial = new THREE.MeshStandardMaterial({
+            map: textureDiff,
+            normalMap: textureNor,
+            aoMap: textureArm,
+            color: '#FFFFFF',
+            metalness: 0.05,
+            roughness: 1,
+            roughnessMap: textureRough,
+            side: THREE.DoubleSide,
+            ambient: new THREE.Color(0x808080)
+        });
+    
+        const floor = new THREE.Mesh(floorGeometry, floorMaterial);
         floor.receiveShadow = true;
         floor.position.y = -7.1;
         floor.quaternion.setFromAxisAngle(new THREE.Vector3(-1, 0, 0), Math.PI * .5);
+    
         const floorBody = new CANNON.Body({
             type: CANNON.Body.STATIC,
             shape: new CANNON.Plane(),
@@ -88,5 +71,14 @@ export default class Floor {
         gameData.physicsWorld.addBody(floorBody);
         gameData.scene.add(floor);
     }
+    
+    loadTexture(url) {
+        const textureSize = 25;
+        const texture = new THREE.TextureLoader().load(url);
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+        texture.repeat.set(textureSize, textureSize);
+        return texture;
+    }
+    
 
 }
--- script/gameLogic.js
+++ script/gameLogic.js
@@ -61,7 +61,7 @@ export function initScene() {
     gameData.scene = new THREE.Scene();
 
     gameData.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, .1, 300)
-    gameData.camera.position.set(15, 25, 35);
+    gameData.camera.position.set(15, 25, 30);
     gameData.camera.rotation.set(-Math.PI * 0.25, Math.PI * 0.1, Math.PI * 0.1);
 
 
